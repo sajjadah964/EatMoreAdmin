a16fbccef88d9573f8030c6f9ad250eaf1444687
Sajjad Ahmad <73050766+sajjadah964@users.noreply.github.com>
2023-05-16 23:09:29 +0500

    Update index.js

--- EatMoreAdmin/src/Screen/index.js
+++ EatMoreAdmin/src/Screen/index.js
@@ -6,7 +6,7 @@ export { default as Login } from './Login/Login';
 export { default as ItemsDetails } from './ItemsDetails/ItemsDetails';
 export { default as Checkout } from './Checkout/Checkout';
 export { default as OrderInformation } from './OrderInformation/OrderInformation';
-export { default as AllOrder } from './AllOrder/AllOrder';
+// export { default as AllOrder } from './AllOrder/AllOrder';
 export { default as Dashboard } from './Dashboard/Dashboard';
 export { default as SplashScreen } from './SplashScreen/SplashScreen';
 
